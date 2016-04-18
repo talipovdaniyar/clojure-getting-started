f86acfc27c71bb477ae8add3bfff3d0446ded428
Daniyar <talipov.daniyar@gmail.com>
2016-04-18 10:31:46 +0200

    Changed dependencies

--- project.clj
+++ project.clj
@@ -8,8 +8,7 @@
                  [ring/ring-jetty-adapter "1.4.0"]
                  [environ "1.0.0"]
                  [org.postgresql/postgresql "9.4-1201-jdbc41"]
-                 [org.clojure/java.jdbc "0.3.6"]
-                 [clojure.jdbc/clojure.jdbc-c3p0 "0.3.1"]]
+                 [org.clojure/java.jdbc "0.4.1"]]
   :min-lein-version "2.0.0"
   :plugins [[environ/environ.lein "0.3.1"]]
   :hooks [environ.leiningen.hooks]

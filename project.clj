6b59eb781ec2976c67d8c5736ada058062827d41
Daniyar <talipov.daniyar@gmail.com>
2016-04-18 02:15:04 +0200

    Final changes

--- project.clj
+++ project.clj
@@ -6,7 +6,9 @@
   :dependencies [[org.clojure/clojure "1.6.0"]
                  [compojure "1.4.0"]
                  [ring/ring-jetty-adapter "1.4.0"]
-                 [environ "1.0.0"]]
+                 [environ "1.0.0"]
+                 [org.clojure/java.jdbc "0.3.6"]
+                 [clojure.jdbc/clojure.jdbc-c3p0 "0.3.1"]]
   :min-lein-version "2.0.0"
   :plugins [[environ/environ.lein "0.3.1"]]
   :hooks [environ.leiningen.hooks]
